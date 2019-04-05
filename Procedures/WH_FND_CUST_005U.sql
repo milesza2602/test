8df5064c71a0a5011f61d2af6057b366587879ba
Project Collection Build Service (MDM) <Build\767377a2-0b93-4a4a-bfa6-70b38056ff73>
2019-04-05 09:45:11 +0000

    ***NO_CI***

--- Procedures/WH_FND_CUST_005U.sql
+++ Procedures/WH_FND_CUST_005U.sql
@@ -17,6 +17,7 @@ set define on;
 --  Maintenance:
 --  Add a comment to see if this will run in dev environment
 --  Add another comment for sanity check
+-- Testing
 --
 -- Note: This version Attempts to do a bulk insert / update / hospital. Downside is that hospital message is generic!!
 --       This would be appropriate for large loads where most of the data is for Insert like with Sales transactions.
